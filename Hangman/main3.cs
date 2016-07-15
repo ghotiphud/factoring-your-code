8a540b08261a709c694c9ed931d0e72a025872f6
Yale Cason <yale.cason@gmail.com>
2016-07-14 22:32:58 -0400

    init presentation copied from rust-intro

---
+++ Hangman/main3.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Hangman{
+    public class Game {
+        private readonly string _wordToGuess;
+        private readonly string _wordToGuessUppercase;
+        private readonly StringBuilder _wordDisplay;
+        private int _lives;
+        private int _lettersRevealed;
+
+        private readonly List<char> _correctGuesses = new List<char>();
+        private readonly List<char> _incorrectGuesses = new List<char>();
+
+        public bool Finished { get { return Won || _lives == 0; } }
+        public bool Won { get { return _lettersRevealed == _wordToGuess.Length; } }
+
+        public Game(string wordToGuess, int lives){
+            _wordToGuess = wordToGuess;
+            _wordToGuessUppercase = wordToGuess.ToUpper();
+            _lives = lives;
+
+            _wordDisplay = new StringBuilder(new String('_', _wordToGuess.Length));
+        }
+
+        public GuessResult MakeGuess(char guess){
+            guess = Char.ToUpper(guess);
+
+            // Not new-ing this struct to allow the 
+            // compiler to catch unset properties.
+            // If GuessType were not set in one of the 
+            // branches it would not compile
+            GuessResult result;
+
+            if (_correctGuesses.Contains(guess))
+            {
+                result.GuessType = GuessType.PreviousCorrect;
+            }
+            else if (_incorrectGuesses.Contains(guess))
+            {
+                result.GuessType = GuessType.PreviousIncorrect;
+            }
+            else if (_wordToGuessUppercase.Contains(guess))
+            {
+                result.GuessType = GuessType.Correct;
+                _correctGuesses.Add(guess);
+
+                RevealLetter(guess);
+            }
+            else
+            {
+                result.GuessType = GuessType.Incorrect;
+                _incorrectGuesses.Add(guess);
+                _lives--;
+            }
+
+            result.WordDisplay = _wordDisplay.ToString();
+            return result;
+        }
+
+        private void RevealLetter(char guess){
+            for (int i = 0; i < _wordToGuess.Length; i++) 
+            {
+                if (_wordToGuessUppercase[i] == guess)
+                {
+                    _wordDisplay[i] = _wordToGuess[i];
+                    _lettersRevealed++;
+                }
+            }
+        }
+    }
+
+    public enum GuessType{
+        None,
+        PreviousCorrect,
+        PreviousIncorrect,
+        Correct,
+        Incorrect,
+    }
+
+    public struct GuessResult{
+        public string WordDisplay { get; set; }
+        public GuessType GuessType { get; set; }
+    }
+
+    public static class Program {
+        public static void Main(string[] args){
+            var random = new Random();
+
+            string[] wordBank = { "Blue", "Black", "Yellow", "Orange", "Green", "Purple" };
+
+            string wordToGuess = wordBank[random.Next(0, wordBank.Length)];
+
+            var game = new Game(wordToGuess, 5);
+
+            string input;
+            char guess;
+
+            while (!game.Finished) 
+            {
+                Console.Write("Guess a letter: ");
+
+                input = Console.ReadLine();
+                guess = input[0];
+
+                var guessResult = game.MakeGuess(guess);
+
+                switch(guessResult.GuessType){
+                    case GuessType.PreviousCorrect:
+                        Console.WriteLine("You've already tried '{0}', and it was correct!", guess);
+                        break;
+                    case GuessType.PreviousIncorrect:
+                        Console.WriteLine("You've already tried '{0}', and it was wrong!", guess);
+                        break;
+                    case GuessType.Correct:
+                        // Just display the updated Word
+                        break;
+                    case GuessType.Incorrect:
+                        Console.WriteLine("Nope, there's no '{0}' in it!", guess);
+                        break;
+                    default:
+                        Trace.Fail("Unexpected GuessType returned");
+                        break;
+                }
+                
+                Console.WriteLine(guessResult.WordDisplay);
+            }
+
+            if (game.Won)
+                Console.WriteLine("You won!");
+            else
+                Console.WriteLine("You lost! It was '{0}'", wordToGuess);
+
+            Console.Write("Press ENTER to exit...");
+            Console.ReadLine();
+        }
+    }
+}
